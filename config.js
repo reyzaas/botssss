d26949442e1333653004bc97e74e62496c3bab05
reyzaas <98476420+reyzaas@users.noreply.github.com>
2022-02-10 21:14:41 +0300

    да

---
+++ config.js
@@ -0,0 +1,13 @@
+const TOKEN = "c9c09bc85d426fbfbc758301ee533814d1fb5e6fff56386dfa6ffd5801951febd5f9c5c4726247b499868";
+const GROUP_ID = 210646810;
+const BTN_TEXT = ["zxczxc??", "сова никогда не спит", "дрейн?", "drain? "];
+const TIME = 150;
+const CHAT_SPAM = `
+so easyyyyyy
+hardy&&???
+`;
+const HELLO_TEXT = `по лезвию в небо...`;
+
+module.exports = {
+    TOKEN, CHAT_SPAM, GROUP_ID, BTN_TEXT, HELLO_TEXT, TIME
+};
