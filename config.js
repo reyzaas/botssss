f4fbedb06333dd279785099e78f6b5b38f67c755
reyzaas <98476420+reyzaas@users.noreply.github.com>
2023-10-19 22:54:40 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
-const TOKEN = "c9c09bc85d426fbfbc758301ee533814d1fb5e6fff56386dfa6ffd5801951febd5f9c5c4726247b499868";
-const GROUP_ID = 210646810;
-const BTN_TEXT = ["Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru", "Закончить рейд vto.pe vkbot.ru"];
+const TOKEN = "25e8da976b7dd02589658993b5ee2466bce3f270e4997da0613d5a2b4a70183917a8bb85178bd24f72563";
+const GROUP_ID = 210278940;
+const BTN_TEXT = ["тихий дом", "сова никогда не спит", "тихий дом", "сова никогда не спит "];
 const TIME = 150;
 const CHAT_SPAM = `
-⚔ This is dadada ⚔ fuck??
-
+хуй 
 `;
-const HELLO_TEXT = `https://github.com/reyzaas / https://vk.com/public210278940`;
+const HELLO_TEXT = `https://vk.com/video498472491_456239208`;
 
 module.exports = {
     TOKEN, CHAT_SPAM, GROUP_ID, BTN_TEXT, HELLO_TEXT, TIME
